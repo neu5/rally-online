583c9e414c559b3a1f0ab2fbfaa2607e3e8f5ff1
neu5 <neus90@gmail.com>
2022-05-25 16:48:26 +0200

    Add template project

---
+++ packages/server/src/index.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath } from "url";
+import { resolve } from "path";
+import express from "express";
+import { createServer } from "http";
+
+const getDirname = (meta: { url: string }) => fileURLToPath(meta.url);
+const rootDir = getDirname(import.meta);
+const distDir = resolve(rootDir, "../../../", "client/dist");
+
+const app = express();
+const httpServer = createServer(app);
+
+app.use(express.static(distDir));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: distDir });
+});
+
+export const server = httpServer.listen(process.env.PORT || 5000);
