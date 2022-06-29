1f1395605d302e91cfdd9bd4ffd467b1c5fc9119
neu5 <neus90@gmail.com>
2022-06-29 15:42:27 +0200

    Basic usage of the NullEngine on the server #34

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,6 +3,21 @@ import { resolve } from "path";
 import express from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import { ArcRotateCamera, NullEngine, Vector3 } from "@babylonjs/core";
+
+import { createScene } from "./scene/scene";
+
+// type CarPosition = {
+//   x: number;
+//   y: number;
+//   z: number;
+// };
+// const carsStartingPositions: Array<CarPosition> = [
+//   { x: 0, y: 5, z: 0 },
+//   { x: 10, y: 5, z: 0 },
+//   { x: -10, y: 5, z: 0 },
+//   { x: 15, y: 5, z: 0 },
+// ];
 
 const getDirname = (meta: { url: string }) => fileURLToPath(meta.url);
 const rootDir = getDirname(import.meta);
@@ -12,8 +27,7 @@ const app = express();
 const httpServer = createServer(app);
 
 interface ServerToClientEvents {
-  playerConnected: (playersList: Object) => void;
-  playerLeft: (playersList: Object) => void;
+  playerListUpdate: (playersList: Object) => void;
   playerID: (id: string) => void;
 }
 const io = new Server<ServerToClientEvents>(httpServer);
@@ -25,7 +39,7 @@ const createSocketHandlers = (socket: Socket) => {
     playersList.delete(socket.id);
 
     io.emit(
-      "playerLeft",
+      "playerListUpdate",
       Array.from(playersList, ([id, data]) => ({ id, data }))
     );
   });
@@ -37,12 +51,29 @@ io.on("connection", (socket) => {
   createSocketHandlers(socket);
 
   io.emit(
-    "playerConnected",
+    "playerListUpdate",
     Array.from(playersList, ([id, data]) => ({ id, data }))
   );
 
   socket.emit("playerID", socket.id);
 });
+
+(async () => {
+  const engine = new NullEngine();
+  const scene = await createScene(engine);
+
+  const camera = new ArcRotateCamera( // eslint-disable-line
+    "camera",
+    -Math.PI / 2,
+    Math.PI / 3.5,
+    130,
+    Vector3.Zero()
+  );
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
+})();
 
 app.use(express.static(distDir));
 
