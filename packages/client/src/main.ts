0d597af823ee2bf3a4289490b20c97083c0e063c
neu5 <neus90@gmail.com>
2023-01-24 15:49:48 +0100

    Comment out not needed things

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,32 +1,31 @@
-import { io } from "socket.io-client";
-import type { Quaternion } from "@babylonjs/core";
-import { ArcRotateCamera, Engine, Scene, Vector3 } from "@babylonjs/core";
-import Toastify from "toastify-js";
+// import type { Quaternion } from "@babylonjs/core";
+// import { ArcRotateCamera, Engine, Scene, Vector3 } from "@babylonjs/core";
+// import Toastify from "toastify-js";
 import "toastify-js/src/toastify.css";
 
 import { createSocketHandler } from "./sockets/sockets";
 
-import { startRace } from "./scene/scene";
-import { UIDialogWrapper, UIcreatePlayersList, UIsetCurrentPlayer } from "./ui";
-import { TOAST_COLORS } from "./utils";
+// import { startRace } from "./scene/scene";
+// import { UIDialogWrapper, UIcreatePlayersList, UIsetCurrentPlayer } from "./ui";
+// import { TOAST_COLORS } from "./utils";
 
-import type {
-  GameConfig,
-  GameObject,
-  Player,
-  Position,
-  ServerToClientEvents,
-} from "@neu5/types/src";
+// import type {
+//   GameConfig,
+//   GameObject,
+//   Player,
+//   Position,
+//   ServerToClientEvents,
+// } from "@neu5/types/src";
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const FPSEl = document.getElementById("fps") as HTMLElement;
-const startRaceBtn = document.getElementById(
-  "start-race-btn"
-) as HTMLAnchorElement;
-const stopRaceBtn = document.getElementById(
-  "stop-race-btn"
-) as HTMLAnchorElement;
-const playersListEl = document.getElementById("players-list") as HTMLElement;
+// const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+// const FPSEl = document.getElementById("fps") as HTMLElement;
+// const startRaceBtn = document.getElementById(
+//   "start-race-btn"
+// ) as HTMLAnchorElement;
+// const stopRaceBtn = document.getElementById(
+//   "stop-race-btn"
+// ) as HTMLAnchorElement;
+// const playersListEl = document.getElementById("players-list") as HTMLElement;
 
 // const throttle = (func: Function, timeFrame: number = 0) => {
 //   var lastTime = 0;
@@ -44,8 +43,8 @@ const playersListEl = document.getElementById("players-list") as HTMLElement;
 // }, 1000);
 
 (async () => {
-  const engine = new Engine(canvas, true);
-  let scene: Scene = new Scene(engine);
+  // const engine = new Engine(canvas, true);
+  // let scene: Scene = new Scene(engine);
 
   createSocketHandler();
 })();
